76c4ac3da2cf2a0b6ec2640f364f4d420334493a
CheechRockwizard <github@lamcat.co.uk>
2023-06-05 16:27:36 +0100

    day 4 pm

--- index.php
+++ index.php
@@ -18,65 +18,40 @@ get_header();
         </div>
     </section>
 
-    <div class="bg--grey py-5">
+    <section class="latest_insights py-5">
         <div class="container-xl">
-            <h2>Latest News</h2>
-            <div class="row w-100" id="grid">
+            <div class="latest_insights__grid">
                 <?php
-    while (have_posts()) {
-        the_post();
-        $img = get_the_post_thumbnail_url(get_the_ID(), 'large');
-        if (!$img) {
-            $img = get_stylesheet_directory_uri() . '/img/default-blog.jpg';
-        }
-        $cats = get_the_category();
-        $category = wp_list_pluck($cats, 'name');
-        $flashcat = cbslugify($category[0]);
-        $catclass = implode(' ', array_map('cbslugify', $category));
-        $category = implode(', ', $category);
-
-        if (has_category('event')) {
-            $the_date = get_field('start_date', get_the_ID());
-        } else {
-            $the_date = get_the_date('jS F, Y');
-        }
-
+while (have_posts()) {
+    the_post();
+    if ($wp_query->current_post == 0 && !is_paged()) {
         ?>
-                <div
-                    class="grid_item col-lg-4 col-md-6 p-0 <?=$catclass?>">
-                    <a href="<?=get_the_permalink(get_the_ID())?>">
-                        <div class="card card--<?=$flashcat?>">
-                            <div class="news__image_container">
-                                <div
-                                    class="news__flash news__flash--<?=$flashcat?>">
-                                    <?=$category?>
-                                </div>
-                                <div class="news__image"
-                                    style="background-image:url('<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>')">
-                                </div>
-                            </div>
-                            <div class="news__inner">
-                                <h3 class="news__title mb-0">
-                                    <?=get_the_title()?>
-                                </h3>
-                                <div class="news__date">
-                                    <?=$the_date?>
-                                </div>
-                                <div class="news__content">
-                                    <div class="news__content__overlay"></div>
-                                    <?=wp_trim_words(get_the_content(get_the_ID()), 20)?>
-                                </div>
-                            </div>
-                            <!-- <div class="card__link">Read more</div> -->
-                        </div>
-                    </a>
+                <div class="latest_insights__card--first"
+                    style="background-image:url(<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>">
+                    <div class="overlay"></div>
+                    <h2><?=get_the_title()?></h2>
+                    <p><?=wp_trim_words(get_the_content(), 20)?></p>
+                </div>
+                <?php
+    } else {
+        ?>
+                <div class="latest_insights__card <?=$card?>">
+                    <img src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>"
+                        alt="">
+                    <h2><?=get_the_title()?></h2>
+                    <p><?=wp_trim_words(get_the_content(), 20)?></p>
+                    <a class="btn btn-outline-secondary"
+                        href="<?=get_the_permalink()?>">Read
+                        more</a>
                 </div>
                 <?php
     }
+}
 ?>
             </div>
         </div>
-    </div>
+    </section>
+
 </main>
 <?php
 
--- single.php
+++ single.php
@@ -2,108 +2,105 @@
 // Exit if accessed directly.
 defined('ABSPATH') || exit;
 get_header();
-$img = get_the_post_thumbnail_url(get_the_ID(),'full');
+$img = get_the_post_thumbnail_url(get_the_ID(), 'full');
 ?>
 <main id="main" class="blog">
     <?php
     $content = get_the_content();
-    $blocks = parse_blocks($content);
-    $sidebar = array();
-    $after;
-    ?>
+$blocks = parse_blocks($content);
+$sidebar = array();
+$after;
+?>
     <section class="breadcrumbs container-xl">
-    <?php
-    if (function_exists('yoast_breadcrumb')) {
-        yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
-    }
-    ?>
+        <?php
+if (function_exists('yoast_breadcrumb')) {
+    yoast_breadcrumb('<p id="breadcrumbs">', '</p>');
+}
+?>
     </section>
     <div class="container-xl">
         <div class="row g-4 pb-4">
             <div class="col-lg-9 order-2">
-                <h1 class="blog__title"><?=get_the_title()?></h1>
                 <img src="<?=$img?>" alt="" class="blog__image">
-            <?php
-            $count = estimate_reading_time_in_minutes( get_the_content(), 200, true, true );
-            echo $count;
+                <h1 class="blog__title"><?=get_the_title()?></h1>
+                <?php
+        $count = estimate_reading_time_in_minutes(get_the_content(), 200, true, true);
+echo $count;
 
-    foreach ($blocks as $block) {
-        if ($block['blockName'] == 'core/heading') {
-            if (!array_key_exists('level', $block['attrs'])) {
-                $heading = strip_tags($block['innerHTML']);
-                $id = acf_slugify($heading);
-                echo '<a id="' . $id . '" class="anchor"></a>';
-                $sidebar[$heading] = $id;
-            }
+foreach ($blocks as $block) {
+    if ($block['blockName'] == 'core/heading') {
+        if (!array_key_exists('level', $block['attrs'])) {
+            $heading = strip_tags($block['innerHTML']);
+            $id = acf_slugify($heading);
+            echo '<a id="' . $id . '" class="anchor"></a>';
+            $sidebar[$heading] = $id;
         }
-        echo render_block($block);
     }
-            ?>
+    echo render_block($block);
+}
+?>
             </div>
             <div class="col-lg-3 order-1">
                 <?php
-                if ($sidebar) {
-                    ?>
+    if ($sidebar) {
+        ?>
                 <div class="sidebar">
-                    <div class="h5 d-none d-lg-block">Quick Links</div>
+                    <div class="h5 d-none d-lg-block text-white">Quick Links</div>
                     <div class="h5 d-lg-none" data-bs-toggle="collapse" href="#links" role="button">Quick Links</div>
                     <div class="collapse d-lg-block" id="links">
                         <?php
-                        foreach ($sidebar as $heading => $id) {
-                            ?>
-                            <li><a href="#<?=$id?>"><?=$heading?></a></li>
-                            <?php
-                        }
-                        ?>
+            foreach ($sidebar as $heading => $id) {
+                ?>
+                        <li><a
+                                href="#<?=$id?>"><?=$heading?></a>
+                        </li>
+                        <?php
+            }
+        ?>
                     </div>
                 </div>
-                    <?php
-                }
-                ?>
+                <?php
+    }
+?>
             </div>
         </div>
-        <section class="related pb-5">
-            <h3><span>Related</span> Guides</h3>
-            <div class="row g-4">
-            <?php
+    </div>
+    <?php
             $cats = get_the_category();
-            $ids = wp_list_pluck($cats,'term_id');
-            $r = new WP_Query(array(
-                'category__in' => $ids,
-                'posts_per_page' => 4
-            ));
-            while ($r->have_posts()) {
-                $r->the_post();
-                ?>
-                <div class="col-md-6 col-xl-3">
-                    <a class="blog_card" href="<?=get_the_permalink()?>">
-                        <img src="<?=get_the_post_thumbnail_url(get_the_ID(),'large')?>" alt="" class="blog_card__image">
-                        <div class="blog_card__content">
-                            <h3 class="blog_card__title"><?=get_the_title()?></h3>
-                        </div>
-                    </a>
+$ids = wp_list_pluck($cats, 'term_id');
+$r = new WP_Query(array(
+    'category__in' => $ids,
+    'posts_per_page' => 4,
+    'post__not_in' => array(get_the_ID())
+));
+if ($r->have_posts()) {
+    ?>
+    <section class="latest_insights py-5">
+        <div class="container-xl">
+            <h2><span>Related</span> Insights</h2>
+            <div class="latest_insights__grid">
+                <?php
+while ($r->have_posts()) {
+    $r->the_post();
+    ?>
+                <div class="latest_insights__card">
+                    <img src="<?=get_the_post_thumbnail_url(get_the_ID(), 'large')?>"
+                        alt="">
+                    <h3><?=get_the_title()?></h3>
+                    <a class="btn btn-outline-secondary"
+                        href="<?=get_the_permalink()?>">Read
+                        more</a>
                 </div>
                 <?php
-            }
-            ?>
-            </div>
-        </section>
-    </div>
-<section class="cta_block cta_block--light py-5">
-    <div class="container-xl">
-        <h2 class="cta__title">Get A <span>Cash Offer</span> For Your Property</h2>
-        <div class="cta__intro mb-4"><?=get_field('cta_intro')?></div>
-        <div class="form-container">
-            <div class="form-inner d-flex flex-wrap gap-4 justify-content-center">
-                <div class="text-center">Get a <span>Free Cash Offer</span> today</div>
-                <div class="d-sm-flex w-100 w-sm-auto">
-                    <input type="text" class="form-control mb-3 mb-sm-0 me-2" placeholder="Enter your Post Code">
-                    <button class="d-block w-100 w-sm-auto d-sm-inline btn btn--accent">Get Offer</button>
-                </div>
+}
+    ?>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+    <?php
+}
+?>
 </main>
 <?php
 get_footer();
+?>
