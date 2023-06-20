563a9422d2faedf525c6ee8fd230a8382a531c4c
CheechRockwizard <github@lamcat.co.uk>
2023-06-20 19:47:04 +0100

    form block theme

--- page-templates/blocks/lc_form_block.php
+++ page-templates/blocks/lc_form_block.php
@@ -2,7 +2,7 @@
 $title = get_field('title') ?: get_field('form_block_title', 'options');
 $content = get_field('intro') ?: get_field('form_block_content', 'options');
 $formid = get_field('form_id') ?: get_field('form_block_form_id', 'options');
-$bg = get_field('theme') == 'dark' ? 'bg--grey' : '';
+$bg = get_field('theme') == 'Dark' ? 'bg--grey' : '';
 ?>
 <section class="form_block py-5 <?=$bg?>">
     <div class="container-xl">
